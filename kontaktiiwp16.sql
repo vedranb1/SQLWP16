50e193604947df5ff5b20f7af24488330e8c8e6d
vedranb1 <33353442+vedranb1@users.noreply.github.com>
2017-11-15 18:55:30 +0100

    kontaktiwp16

---
+++ kontaktiiwp16.sql
@@ -0,0 +1,20 @@
+drop database if exists edunovawp16;
+create database edunovawp16;
+
+use edunovawp16;
+
+create table osoba(
+sifra int not null primary key auto_increment,
+ime varchar(50) not null,
+spol boolean,
+datumrodenja datetime not null,
+nadimak varchar(50)
+);
+
+create table kontakt(
+sifra int not null primary key auto_increment,
+osoba int not null,
+telefon varchar(20)
+);
+
+alter table kontakt add foreign key(osoba) references osoba(sifra);
